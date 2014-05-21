7dd52b1b93ae72b0b7c28a8d7735481c5ab70264
Oliver Hidalgo <riquelme.oliver@gmail.com>
2014-05-21 17:21:36 -0400

    Se deja doc de ARQ y fuentes de prod

--- Sprint 1/Grupo III/Proyecto/Revalora/src/main/java/cl/usach/infraestructura/util/FileChangeListener.java
+++
@@ -1,8 +0,0 @@
-package cl.usach.infraestructura.util;
-
-public interface FileChangeListener {
-    /** Invoked when a file changes.   
-     * @param fileName name of changed file.
-     */
-    public void fileChanged(String fileName);
-}
